2519e3630953525c46a8e33b0b79be73bcdc8ac1
Louis896 <weiheng.xiao@mail.mcgill.ca>
2023-10-17 14:22:50 -0400

    updates in setup

--- features/empty.feature
+++
@@ -1,7 +0,0 @@
-Feature: Empty
-
-Scenario: Empty feature
-
-Given I am on the home page
-When I don't do anything
-Then I should still be on the home page
--- features/steps/empty.steps.js
+++
@@ -1,7 +0,0 @@
-const { Given, When, Then, After } = require("cucumber");
-
-// Ref: https://www.testim.io/blog/cucumber-js-for-bdd-an-introductory-tutorial-with-examples/
-
-Given("I am on the home page", async () => {
-
-})
