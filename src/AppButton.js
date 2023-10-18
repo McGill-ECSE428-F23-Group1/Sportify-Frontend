8bd56623b9179cca60559550d419586c027b5744
NeelFaucher <neel.faucher@mail.mcgill.ca>
2023-10-18 16:16:46 -0400

    main profile page - need to fix buttons

---
+++ src/AppButton.js
@@ -0,0 +1,27 @@
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+
+import {colors, fonts} from './constants';
+
+const AppButton = ({title, onPress}) => {
+    return (
+        <TouchableOpacity
+            style={styles.button}
+            onPress={onPress}
+        >
+            <Text style={fonts.button_text}>{title}</Text>
+        </TouchableOpacity>
+    );
+}
+
+const styles = StyleSheet.create({
+    button: {
+        flex: 1,
+        backgroundColor: colors.blue,
+        //borderRadius: 5,
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+      },
+});
+
+export default AppButton;
