aad34a3b2ece9fd511d4eb40dfee5ee55d55511f
Louis-2024 <weiheng.xiao@mail.mcgill.ca>
2023-11-07 19:46:06 -0500

    updates

--- src/screens/ExploreScreen.js
+++ src/screens/ExploreScreen.js
@@ -10,6 +10,8 @@ import { createUser, getUser, deleteUser } from '../../features/steps/utils';
 const ExploreScreen = ({route, navigation, accountUsername}) => {
 
     const [allAccounts, setAllAccounts] = useState([]);
+    const [searchBarText, onChangeSearchBarText] = useState("");
+    const [searchText, setSearchText] = useState("");
 
     useEffect(() => {
         const getAllAccounts = async () => {
@@ -35,40 +37,57 @@ const ExploreScreen = ({route, navigation, accountUsername}) => {
 
     return(
         <View style={styles.container}>
-            <ScrollView style={styles.list_container}> 
-                <FlatList
-                    data={allAccounts}
-                    keyExtractor={item => item.username}
-                    renderItem={({item}) => 
-                        <TouchableOpacity style={[styles.list_card, {height: 50+10*item.sports.length, display: item.username==accountUsername? "none":"flex"}]}>
-                            <View style={[styles.card_text, {height:50+10*item.sports.length}]}>
-                                <Text style={fonts.card_title}>{item.username}</Text>
-                                <FlatList
-                                    data={item.sports}
-                                    keyExtractor={item => item.id}
-                                    renderItem={({item}) => 
-                                        <View>
-                                            <Text style={fonts.card_text}>{item.sportName.toUpperCase()} - {item.sportLevel}</Text>
-                                        </View>
-                                    }
-                                />
-                            </View>
-                            <View style={[styles.card_buttons, {height:50+10*item.sports.length}]}>
-                                <View style={styles.card_button_container}>
-                                    <TouchableOpacity style={styles.card_button} onPress={()=>pressAddFriend(item.username)}>
-                                        <MaterialCommunityIcons name={"account-plus"} size={25}/>
-                                    </TouchableOpacity>
-                                </View>
-                                <View style={styles.card_button_container}>
-                                    <TouchableOpacity style={styles.card_button} onPress={()=>pressChat(item.username)}>
-                                        <MaterialCommunityIcons name={"message-processing-outline"} size={25}/>
-                                    </TouchableOpacity>
-                                </View>
-                            </View>
-                        </TouchableOpacity>
-                    }
+            <View style={styles.search_bar}>
+                <TextInput 
+                    onChangeText={onChangeSearchBarText}
+                    value={searchBarText}
+                    style={styles.search_bar_text}
+                    autoCapitalize={'none'}
+                    autoCorrect={false}
+                    spellCheck={false}
+                    maxLength={20}
                 />
-            </ScrollView>
+                <TouchableOpacity style={styles.search_bar_button} onPress={()=>{setSearchText(searchBarText)}}>
+                    <MaterialCommunityIcons name={"magnify"} size={25}/>
+                </TouchableOpacity>
+            </View>
+            <View style={{marginTop: 10}}>
+                <ScrollView style={styles.list_container}> 
+                    <FlatList
+                        data={allAccounts}
+                        keyExtractor={item => item.username}
+                        renderItem={({item}) => 
+                            <TouchableOpacity style={[styles.list_card, {height: 50+10*item.sports.length, 
+                            display: ((item.username!=accountUsername)&&((searchText=="")||(item.username.includes(searchText))))? "flex":"none"}]}>
+                                <View style={[styles.card_text, {height:50+10*item.sports.length}]}>
+                                    <Text style={fonts.card_title}>{item.username}</Text>
+                                    <FlatList
+                                        data={item.sports}
+                                        keyExtractor={item => item.id}
+                                        renderItem={({item}) => 
+                                            <View>
+                                                <Text style={fonts.card_text}>{item.sportName.toUpperCase()} - {item.sportLevel}</Text>
+                                            </View>
+                                        }
+                                    />
+                                </View>
+                                <View style={[styles.card_buttons, {height:50+10*item.sports.length}]}>
+                                    <View style={styles.card_button_container}>
+                                        <TouchableOpacity style={styles.card_button} onPress={()=>pressAddFriend(item.username)}>
+                                            <MaterialCommunityIcons name={"account-plus"} size={25}/>
+                                        </TouchableOpacity>
+                                    </View>
+                                    <View style={styles.card_button_container}>
+                                        <TouchableOpacity style={styles.card_button} onPress={()=>pressChat(item.username)}>
+                                            <MaterialCommunityIcons name={"message-processing-outline"} size={25}/>
+                                        </TouchableOpacity>
+                                    </View>
+                                </View>
+                            </TouchableOpacity>
+                        }
+                    />
+                </ScrollView>
+            </View>
         </View>
     );
 };
@@ -76,11 +95,12 @@ const ExploreScreen = ({route, navigation, accountUsername}) => {
     const styles = StyleSheet.create({
         container: {
             flex: 1,
-            backgroundColor: colors.light_blue
+            backgroundColor: colors.light_blue,
+            margin: 20,
+            flexDirection: 'column'
         },
         list_container: {
             flex: 1,
-            margin: 20
         },
         list_card:{
             width: '100%',
@@ -104,12 +124,35 @@ const ExploreScreen = ({route, navigation, accountUsername}) => {
             justifyContent: "center"
         },
         card_button:{
-            width:36, 
-            height:36, 
+            width:32, 
+            height:32, 
             borderRadius:18,
             alignItems: "center",
             justifyContent: "center",
             backgroundColor: colors.white
+        },
+        search_bar:{
+            width: '100%',
+            height: 32,
+            backgroundColor: colors.white,
+            flexDirection: 'row'
+        },
+        search_bar_text:{
+            flex:1,
+            marginRight: 32,
+            marginLeft: 6,
+            marginVertical: 6,
+            fontSize: 15,
+            fontWeight: '500',
+        },
+        search_bar_button:{
+            backgroundColor: colors.blue,
+            width: 32,
+            height: 32,
+            alignItems: "center",
+            justifyContent: "center",
+            position: 'absolute',
+            right: 0
         }
 
     })
